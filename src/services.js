185570d7b6ff1ac7189faa8ee169bd408a901acf
irinatiron <irinatirongiurgiu@gmail.com>
2025-07-04 13:16:50 +0200

    new style.css

--- src/services.js
+++ src/services.js
@@ -16,8 +16,10 @@ async function printMovies() {
   const movies = await getMovies();
   const movieList = movies.map(movie => {
     return moviesContainer.innerHTML += 
-    `<h1>${movie.title}</h1>
-    <p>${movie.director}, ${movie.year}</p>`;
+    `<div class="movieCard">
+    <img src="${movie.poster}" alt="${movie.title}" class="moviePoster">
+    <h3>${movie.title} (${movie.enTitle})</i></h3>
+    <p>Directed by ${movie.director}, ${movie.year}</p></div>`;
   });
   return movieList;
 }
